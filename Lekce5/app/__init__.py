1886b652e6ccca5020cf7fb0ccf89604e81189e6
PM <pemag2027@gmail.com>
2026-04-29 20:33:44 +0200

    Lekce5

---
+++ Lekce5/app/__init__.py
@@ -0,0 +1,6 @@
+from .app import nasobeni
+
+__all__ = ['nasobeni']
+__version__ = '0.1.0'
+__author__ = 'PM'
+__email__ = 'pemag2026'@gmail.com
---
+++ Lekce5/app/calculator.py
@@ -0,0 +1,17 @@
+def nasobeni(a, b):
+
+    """Funkce pro násobení dvou čísel"""
+
+    return a * b
+
+
+def main():
+
+    result = nasobeni(2, 3)
+
+    print(f"Výsledek: {result}")
+
+
+if __name__ == "__main__":
+
+    main()
