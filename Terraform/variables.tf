7dfe1199fbbe5fae05b54d59591a84d7856a6472
pemag2027 <pemag2027@gmail.com>
2026-04-30 13:50:19 +0200

    Create variables.tf

---
+++ Terraform/variables.tf
@@ -0,0 +1,20 @@
+variable "aws_region" {
+  description = "AWS region"
+  type = string
+  default = "eu-central-1"
+}
+
+
+variable "repository_name" {
+  description = "Name of the ECR repository"
+  type = string
+  default = "python-calculator"
+}
+
+
+
+variable "environment" {
+  description = "Environment name"
+  type = string
+  default = "dev"
+}
